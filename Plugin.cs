e244059e91fbf7f2a3f6c6493e7cafe69a00ce43
MetaMiku <MetaMiku@hotmail.com>
2025-11-16 14:00:37 +0800

    init

---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using UnityEngine;
+using UnityEngine.UI;
+using HarmonyLib;
+using System.Runtime.Serialization.Formatters;
+using System.Collections.Generic;
+
+namespace MetaMystia;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log = base.Log;
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
