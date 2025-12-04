ad53e6b095cdd1ab21c949d9584dfbb4c0b8d539
MetaMiku <MetaMiku@hotmail.com>
2025-12-04 17:35:36 +0800

    feat: refine GetMapNameCN

--- Utils.cs
+++ Utils.cs
@@ -11,17 +11,27 @@ public static class Utils
     {
         return mapLabel switch
         {
-            "Home" => "家",
-            "MoveCharacter" => "地下室",
-            "BeastForest" => "兽道",
+            "Home" => "夜雀小屋",
+            "Basement" => "地下室",
+            "BeastForest" => "妖怪兽道",
             "HumanVillage" => "人间之里",
             "HakureiShrine" => "博丽神社",
+            "ScarletMansion" => "红魔馆",
+            "BambooForest" => "迷途竹林",
+            "PartyStage" => "舞台",
+            "Hakugyokurou" => "白玉楼",
             "DLC1_MagicForest" => "魔法森林",
             "DLC1_YoukaiMountain" => "妖怪之山",
             "DLC2_FormerHell" => "旧地狱",
             "DLC2_EarthSpiritsPalace" => "地灵殿",
             "DLC3_MyourenTemple" => "命莲寺",
-            // 不会拼斯卡雷特，其他的没开暂时
+            "DLC3_DivineSpiritMausoleum" => "神灵庙",
+            "DLC3_HakureiFestival" => "博丽大祭",
+            "DLC4_GardenOfTheSun" => "太阳花田",
+            "DLC4_ShiningNeedleCastle" => "辉针城",
+            "DLC4_ScarletMansionBasement" => "红魔馆地下室",
+            "DLC5_Makai" => "魔界",
+            "DLC5_LunarCapital" => "月之都",
             _ => mapLabel
         };
     }
