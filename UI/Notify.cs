ac8ddd02828084402eaae5025651855372cf4543
ShigureYuki <shigure.yuki@qq.com>
2026-01-11 22:20:58 +0800

    Fix: Enhanced Notify.Show

--- UI/Notify.cs
+++ UI/Notify.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Common.UI;
 
 namespace MetaMystia;
@@ -5,10 +7,14 @@ namespace MetaMystia;
 [AutoLog]
 public static partial class Notify
 {
+    public readonly static List<Scene> NotAvailableScenes = [Scene.IzakayaPrepScene, Scene.LoadScene, Scene.MainScene];
+    public static bool CanNotify(Scene scene) => !NotAvailableScenes.Any((s) => s == scene);
+    public static bool CanNotify() => CanNotify(MpManager.LocalScene);
 
     // Note: should run on main thread
     public static void Show(string text)
     {
+        if (!CanNotify()) return;
         try
         {
             ReceivedObjectDisplayerController.Instance.NotifyTextMessage(text);
